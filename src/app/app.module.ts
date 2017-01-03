af81056c5d1c9c87180a94f0fb65f96a5905d722
Harrie Essing <harrie.essing@gmail.com>
2017-01-03 21:27:08 +0100

    Added Authentication pages and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,24 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { SignupPage } from '../pages/signup/signup';
+import { LoginPage } from '../pages/login/login';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
 import { UsersPage } from '../pages/users/users';
 import { ReposPage } from '../pages/repos/repos';
 import { OrganisationsPage } from '../pages/organisations/organisations';
 import { UserDetailsPage } from '../pages/user-details/user-details';
 
+import { AuthData } from '../providers/auth-data';
+import { ProfileData } from '../providers/profile-data';
 import { GithubUsers } from '../providers/github-users';
 
 @NgModule({
   declarations: [
     MyApp,
+    SignupPage,
+    LoginPage,
+    ResetPasswordPage,
     UsersPage,
     ReposPage,
     OrganisationsPage,
@@ -23,11 +31,19 @@ import { GithubUsers } from '../providers/github-users';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    SignupPage,
+    LoginPage,
+    ResetPasswordPage,
     UsersPage,
     ReposPage,
     OrganisationsPage,
     UserDetailsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, GithubUsers]
+  providers: [
+    { provide: ErrorHandler, useClass: IonicErrorHandler }, 
+    AuthData,
+    ProfileData,
+    GithubUsers
+  ]
 })
-export class AppModule {}
+export class AppModule { }
